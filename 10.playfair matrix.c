d0cc8e87681e5ece3c8808fe48516ebb439558a7
vbhargavkumarreddy <133644412+vbhargavkumarreddy@users.noreply.github.com>
2024-09-24 06:47:15 +0530

    Update 10.playfair matrix.c

--- 10.playfair matrix.c
+++ 10.playfair matrix.c
@@ -1,94 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
-#define SIZE 100
-void preparePlainText(char *plaintext) 
-{
-    int len=strlen(plaintext);
-    for(int i=0;i<len;i++) 
-    {
-        if(!isalpha(plaintext[i])) 
-        {
-            for(int j=i;j<len;j++) 
-            {
-                plaintext[j]=plaintext[j+1];
-            }
-            len--;
-            i--;
-        } 
-        else if(plaintext[i]=='J') 
-        {
-            plaintext[i]='I';
+#define MAX_LENGTH 100
+void prepareText(char *input, char *output) {
+    int j = 0;
+    for (int i = 0; input[i]; i++) {
+        if (input[i] != ' ') {
+            output[j++] = toupper(input[i]);
+            if (input[i] == input[i + 1]) output[j++] = 'X';
         }
     }
-    if (len%2!=0) 
-    {
-        plaintext[len++]='X';
-    }
-    plaintext[len]='\0';
+    if (j % 2) output[j++] = 'X'; // Add 'X' if odd length
+    output[j] = '\0';
 }
-void findPosition(char matrix[5][5],char letter,int *row,int *col) 
-{
-    if(letter=='J') 
-    {
-        letter='I';
+void encodePlayfair(char *text) {
+    char key[5][5] = {
+        {'M', 'O', 'N', 'A', 'R'},
+        {'C', 'H', 'Y', 'B', 'D'},
+        {'E', 'F', 'G', 'I', 'K'},
+        {'L', 'P', 'Q', 'S', 'T'},
+        {'U', 'V', 'W', 'X', 'Z'}
+    };   
+    char formatted[MAX_LENGTH];
+    prepareText(text, formatted);
+    for (int i = 0; formatted[i]; i += 2) {
+        int r1, c1, r2, c2;
+        for (int r = 0; r < 5; r++)
+            for (int c = 0; c < 5; c++)
+                if (key[r][c] == formatted[i]) { r1 = r; c1 = c; }
+                else if (key[r][c] == formatted[i + 1]) { r2 = r; c2 = c; }
+        printf("%c%c", 
+            r1 == r2 ? key[r1][(c1 + 1) % 5] : (c1 == c2 ? key[(r1 + 1) % 5][c1] : key[r1][c2]),
+            r2 == r1 ? key[r2][(c2 + 1) % 5] : (c2 == c1 ? key[(r2 + 1) % 5][c2] : key[r2][c1])
+        );
     }
-    for(*row=0;*row<5;(*row)++) 
-    {
-        for(*col=0;*col<5;(*col)++) 
-        {
-            if(matrix[*row][*col]==letter) 
-            {
-                return;
-            }
-        }
-    }
+    printf("\n");
 }
-void encryptPlayfair(char *plaintext,char matrix[5][5],char *encryptedText) 
-{
-    int len=strlen(plaintext);
-    int row1,col1,row2,col2;
-    int index=0;
-    for(int i=0;i<len;i+=2) 
-    {
-        findPosition(matrix,plaintext[i],&row1,&col1);
-        findPosition(matrix,plaintext[i+1],&row2,&col2);
-
-        if(row1==row2) 
-        {
-            encryptedText[index++]=matrix[row1][(col1+1)%5];
-            encryptedText[index++]=matrix[row2][(col2+1)%5];
-        } 
-        else if(col1==col2) 
-        {
-            encryptedText[index++]=matrix[(row1+1)%5][col1];
-            encryptedText[index++]=matrix[(row2+1)%5][col2];
-        } 
-        else 
-        {
-            encryptedText[index++]=matrix[row1][col2];
-            encryptedText[index++]=matrix[row2][col1];
-        }
-    }
-    encryptedText[index]='\0';
-}
-int main() 
-{
-    char keyMatrix[5][5] = 
-    {
-        {'M','F','H','I','K'},
-        {'U','N','O','P','Q'},
-        {'Z','V','W','X','Y'},
-        {'E','L','A','R','G'},
-        {'D','S','T','B','C'}
-    };
-    char plaintext[] ="Must see you over Cadogan West. Coming at once";
-    char processedText[SIZE];
-    char encryptedText[SIZE*2];
-    strcpy(processedText,plaintext);
-    preparePlainText(processedText);
-    encryptPlayfair(processedText,keyMatrix,encryptedText);
-    printf("\nPlaintext = %s\n",plaintext);
-    printf("\nEncrypted text = %s\n",encryptedText);
+int main() {
+    char plaintext[MAX_LENGTH] = "Must see you over Cadogan West";
+    printf("Encoded Message: ");
+    encodePlayfair(plaintext);
+    return 0;
 }
